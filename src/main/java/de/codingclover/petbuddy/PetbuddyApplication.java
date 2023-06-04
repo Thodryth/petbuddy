f0849bcbefe3d8ab1bf33967cff0d0f2e18acecd
Christopher Kleemann <christopher.kleemann@gmx.de>
2023-06-04 09:48:19 +0200

    init spring project

---
+++ src/main/java/de/codingclover/petbuddy/PetbuddyApplication.java
@@ -0,0 +1,13 @@
+package de.codingclover.petbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetbuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/codingclover/petbuddy/PetbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package de.codingclover.petbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
