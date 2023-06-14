b5fa6bfd5b914b6f6a9ea59397fbc3eb7d834267
Christopher Kleemann <christopher.kleemann@gmx.de>
2023-06-14 14:30:52 +0200

    Removed unnacessary annotation

--- src/main/java/de/codingclover/petbuddy/persistence/repository/AnimalRepository.java
+++ src/main/java/de/codingclover/petbuddy/persistence/repository/AnimalRepository.java
@@ -3,11 +3,9 @@ package de.codingclover.petbuddy.persistence.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import de.codingclover.petbuddy.persistence.entity.Animal;
 
-@Repository
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
 
     List<Animal> findAllByOwner(String owner);
